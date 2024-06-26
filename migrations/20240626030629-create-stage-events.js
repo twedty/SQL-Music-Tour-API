6dd8d82e510ed94497695007fcbffa1709641d31
twedty <jamestwedt@gmail.com>
2024-06-25 23:06:46 -0400

    added stage_events model

---
+++ migrations/20240626030629-create-stage-events.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('stage_events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      stage_events_id: {
+        type: Sequelize.INTEGER
+      },
+      stage_id: {
+        type: Sequelize.SMALLINT
+      },
+      event_id: {
+        type: Sequelize.SMALLINT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('stage_events');
+  }
+};
