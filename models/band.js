085b3f624791db3accfc8d473e6eb33653ae9bc5
twedty <jamestwedt@gmail.com>
2024-06-25 22:24:11 -0400

    generated a model in bands.js

--- models/band.js
+++ models/band.js
@@ -1,37 +1,45 @@
-// DEPENDENCIES
-const { Sequelize, DataTypes,  Model } = require('sequelize')
-
-// MODEL
-class Band extends Model{}
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Band extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+    static associate(models) {
+      // define association here
+    }
+}
 Band.init({
-    band_id: { 
-        type: DataTypes.INTEGER, 
-        primaryKey: true, 
+    band_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
         autoIncrement: true
     },
-    name: { 
-        type: DataTypes.STRING, 
-        allowNull: false 
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
     },
-    genre: { 
-        type: DataTypes.TEXT, 
-        allowNull: false 
+    genre: {
+        type: DataTypes.TEXT,
+        allowNull: false
     },
-    available_start_time: { 
-        type: DataTypes.DATE, 
-        allowNull: false 
+    available_start_time: {
+        type: DataTypes.DATE,
+        allowNull: false
     },
-    end_time: { 
-        type: DataTypes.DATE, 
-        allowNull: false 
-    },
-}, {
+    end_time: {
+        type: DataTypes.DATE,
+        allowNull: false
+    }
+    }, {
     sequelize,
     modelName: 'Band',
-    tableName: 'band',
+    tableName: 'bands',
     timestamps: false
-}) 
-
-
-// EXPORT
-module.exports = Band
+    })
+return Band;
+};
