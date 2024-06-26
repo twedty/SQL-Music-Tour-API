5bc7fcbe5bac90056c9c9c54a0afbae435b4a2e7
twedty <jamestwedt@gmail.com>
2024-06-25 23:01:43 -0400

    added meet_greets model

---
+++ migrations/20240626030039-create-meet-greets.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('meet_greets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      meet_greet_id: {
+        type: Sequelize.INTEGER
+      },
+      event_id: {
+        type: Sequelize.SMALLINT
+      },
+      band_id: {
+        type: Sequelize.SMALLINT
+      },
+      meet_start_time: {
+        type: Sequelize.DATE
+      },
+      meet_end_time: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('meet_greets');
+  }
+};
