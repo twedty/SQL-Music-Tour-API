3abd3c2f793a42c408ec7919440fca452f054ce7
twedty <jamestwedt@gmail.com>
2024-06-25 23:05:21 -0400

    added set_time model

---
+++ migrations/20240626030403-create-set-time.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('set_times', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      event_id: {
+        type: Sequelize.SMALLINT
+      },
+      stage_id: {
+        type: Sequelize.SMALLINT
+      },
+      band_id: {
+        type: Sequelize.SMALLINT
+      },
+      start_time: {
+        type: Sequelize.DATE
+      },
+      end_time: {
+        type: Sequelize.DATE
+      },
+      set_time_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('set_times');
+  }
+};
