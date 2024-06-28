9f2391b02a8924bc2cce87e71cabc7d52023cf3e
twedty <jamestwedt@gmail.com>
2024-06-27 20:56:27 -0400

    added finding bands by start time and a query to filter band names

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -2,6 +2,23 @@
 const bands = require('express').Router()
 const db = require('../models')
 const { Band } = db 
+const { Op } = require('sequelize')
+
+// FIND ALL BANDS
+bands.get('/', async (req, res) => {
+    try {
+        const foundBands = await Band.findAll({
+            order: [ [ 'available_start_time', 'ASC' ] ],
+            where: {
+                name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%` }
+            }
+        })
+        res.status(200).json(foundBands)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
 
 // CREATE A BAND
 bands.post('/', async (req, res) => {
@@ -48,16 +65,6 @@ bands.delete('/:id', async (req, res) => {
     }
 })
 
-// FIND ALL BANDS
-bands.get('/', async (req, res) => {
-    try {
-        const foundBands = await Band.findAll()
-        res.status(200).json(foundBands)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
-
 // FIND A SPECIFIC BAND
 bands.get('/:id', async (req, res) => {
     try {
@@ -69,9 +76,6 @@ bands.get('/:id', async (req, res) => {
         res.status(500).json(error)
     }
 })
-
-
-
 
 // EXPORT
 module.exports = bands
