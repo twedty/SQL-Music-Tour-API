bcfce23662ee2dc914f78c54b0d5252e5df54584
twedty <jamestwedt@gmail.com>
2024-06-27 20:53:11 -0400

    added create, update, and delete bands

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -3,6 +3,51 @@ const bands = require('express').Router()
 const db = require('../models')
 const { Band } = db 
 
+// CREATE A BAND
+bands.post('/', async (req, res) => {
+    try {
+        const newBand = await Band.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new band',
+            data: newBand
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// UPDATE A BAND
+bands.put('/:id', async (req, res) => {
+    try {
+        const updatedBands = await Band.update(req.body, {
+            where: {
+                band_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully updated ${updatedBands} band(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// DELETE A BAND
+bands.delete('/:id', async (req, res) => {
+    try {
+        const deletedBands = await Band.destroy({
+            where: {
+                band_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedBands} band(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
 // FIND ALL BANDS
 bands.get('/', async (req, res) => {
     try {
@@ -12,6 +57,20 @@ bands.get('/', async (req, res) => {
         res.status(500).json(error)
     }
 })
+
+// FIND A SPECIFIC BAND
+bands.get('/:id', async (req, res) => {
+    try {
+        const foundBand = await Band.findOne({
+            where: { band_id: req.params.id }
+        })
+        res.status(200).json(foundBand)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+
 
 
 // EXPORT
